50df1c1226b121736efb4e78e0cc5807b114dbe3
David Perez <dpere048@wch136-05.cs.ucr.edu>
2020-05-10 20:40:09 -0700

    practice 1 done

--- header/simAVRHeader.h
+++ header/simAVRHeader.h
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #ifndef F_CPU
 #define F_CPU 8000000
+#ifndef __io_h__
+#define __io_h__
+
+void LCD_init();
+void LCD_ClearScreen(void);
+void LCD_WriteCommand (unsigned char Command);
+void LCD_Cursor (unsigned char column);
+void LCD_WriteData (unsigned char Data);
+void LCD_DisplayString(unsigned char column ,const unsigned char *string);
+void delay_ms(int miliSec);
 #endif
 
 #include <avr/sleep.h>
